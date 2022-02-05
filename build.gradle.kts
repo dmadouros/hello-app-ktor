ea3d967b90482dd1a39ba2d5f11a3c9637af4283
David Madouros <david.madouros@gmail.com>
2022-02-05 15:41:01 -0700

    Add liquibase

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     application
     kotlin("jvm") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.liquibase.gradle") version "2.1.1"
 }
 
 group = "com.kivid"
@@ -28,6 +29,12 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
+    // Liquibase / Postgres (CLI)
+    liquibaseRuntime("info.picocli:picocli:4.6.2")
+    liquibaseRuntime("org.liquibase:liquibase-core:4.7.1")
+    liquibaseRuntime("org.postgresql:postgresql:42.3.2")
+    liquibaseRuntime("org.yaml:snakeyaml:1.30")
+
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
@@ -37,5 +44,17 @@ tasks {
         manifest {
             attributes(Pair("Main-Class", "io.ktor.server.netty.EngineMain"))
         }
+    }
+}
+
+liquibase {
+    activities.register("master") {
+        arguments = mapOf(
+            "classpath" to "src/main/resources",
+            "changeLogFile" to "db/changelog/db.changelog-master.yml",
+            "username" to "hello",
+            "password" to "hello",
+            "url" to "jdbc:postgresql://localhost:5432/hello"
+        )
     }
 }
